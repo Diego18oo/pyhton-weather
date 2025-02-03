74ba1f7beb602167bf62c51bf5b9f2b6263b7be6
Diego18oo <diegorodriguez300806@gmail.com>
2025-02-03 10:05:40 -0600

    Changed units

--- weather.py
+++ weather.py
@@ -6,9 +6,9 @@ import os
 load_dotenv()
 
 
-def get_current_weather(city="Kansas City"):
+def get_current_weather(city="Jalisco"):
 
-    request_url = f'http://api.openweathermap.org/data/2.5/weather?appid={os.getenv("API_KEY")}&q={city}&units=imperial'
+    request_url = f'http://api.openweathermap.org/data/2.5/weather?appid={os.getenv("API_KEY")}&q={city}&units=standard'
 
     weather_data = requests.get(request_url).json()
 
